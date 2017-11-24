818f0ac32201bc7e097992275179303e11823357
ishengtsai <ishengtsai@gmail.com>
2017-11-24 15:40:47 +0000

    Tainan

--- runSmaltPE.sh
+++ runSmaltPE.sh
@@ -8,16 +8,18 @@ fi
 
 
 SAMPLE=$1
-DATA=$2
-REF=$3
+FORWARD=$2
+REVERSE=$3
+REF=$4
+CPU=$5
 
 #looks like this
 #LGC17_MJ17_497bp_TAGCTT_L001_R1.fastq.gz
 
 
 smalt index -k 20 -s 13 $REF $REF
-smalt map -i 10000 -n 8 -x -f samsoft -o $SAMPLE.sam $REF $DATA\_R1.fastq.gz $DATA\_R2.fastq.gz
-samtools sort -@ 8 -O bam -o $SAMPLE.sorted.bam $SAMPLE.sam
+smalt map -i 10000 -n $CPU -x -f samsoft -o $SAMPLE.sam $REF $FORWARD $REVERSE
+samtools sort -@ $CPU -O bam -o $SAMPLE.sorted.bam $SAMPLE.sam
 
 
 #java -Xmx6g -jar /usr/local/bioinfo/picard-tools-1.130/picard.jar MarkDuplicates METRICS_FILE=metrics CREATE_INDEX=true INPUT=$SAMPLE.sorted.bam OUTPUT=$SAMPLE.sorted.markdup.bam
