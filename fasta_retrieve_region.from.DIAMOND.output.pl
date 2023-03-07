ddae6f298300f3ab54404f1ca039cd108efc568e
ishengtsai <ishengtsai@gmail.com>
2023-03-07 09:07:46 +0800

    update

--- fasta_retrieve_region.from.DIAMOND.output.pl
+++ fasta_retrieve_region.from.DIAMOND.output.pl
@@ -9,26 +9,26 @@ my $contig = '';
 
 
 if (@ARGV < 2) {
-	print "fasta_retrieve_region.pl fasta Region\n\n" ;
+	print "fasta_retrieve_region.pl fasta RegionfileFromDIAMOND\n\n" ;
 	exit ;
 }
 
 my $filenameA = shift @ARGV;
 
-my $region = shift @ARGV;
+my $regionFile = shift @ARGV;
 
+my @regions = () ;
 
+open (IN, "$regionFile") or die "oops!\n" ;
+while (<IN>) {
+    my @r = split /\s+/, $_ ;
 
-my $contig_name = '';
-my $start = '';
-my $end = '';
+    push(@regions, "$r[0]\:$r[1]\:$r[6]-$r[7]") ; 
 
-#ref2.scaff0016:900000-950200
-if ( $region =~ /(\S+)\:(\d+)-(\d+)/ ) {
-    $contig_name = $1 ;
-    $start = $2 ;
-    $end = $3 ; 
 }
+close(IN) ; 
+
+
 
 
 my %contig_seq = () ;
@@ -70,10 +70,50 @@ close(IN) ;
 $contig_seq{$read_name} = $read_seq ;
 
 
-print ">$contig_name:$start-$end\n" ;
-$region = substr($contig_seq{$contig_name}, ($start-1), ($end-$start+1) ) ;
-print "$region\n" ;
 
+foreach my $region (@regions) {
 
+    my $contig_name ; my $gene ; my $start ; my $end ; 
+    
+    if ( $region =~ /(\S+)\:(\S+)\:(\d+)-(\d+)/ ) {
+	$contig_name = $1 ;
+	$gene = $2 ; 
+	$start = $3 ;
+	$end = $4 ;
+    }
 
+    if ( -d $gene ) {
+
+    }
+    else {
+	mkdir ($gene) ; 
+    }
+
+    open OUT,">", "$gene/diamond.$contig_name.$gene.$start.$end.nuc.fa" or die "dasdiaodsias\n" ;
+    print OUT ">$contig_name.$gene.$start.$end\n" ;
+
+    print "$region done!\n" ; 
+    
+    if ($start > $end ) {
+	$region = substr($contig_seq{$contig_name}, ($end-1), ($start-$end+1) ) ;
+	$region = revcomp($region) ; 
+    }
+    else {
+	$region = substr($contig_seq{$contig_name}, ($start-1), ($end-$start+1) ) ;
+
+    }
+
+    print OUT "$region\n" ; 
+    close (OUT) ; 
+}
+    
+
+sub revcomp {
+  my $dna = shift;
+  my $revcomp = reverse($dna);
+
+  $revcomp =~ tr/ACGTacgt/TGCAtgca/;
+
+  return $revcomp;
+}
 
